659d9ab0ebba83c951451acc07be0ac1140a5bb9
fcarlosmonteiro <fcarlosmonteiro@gmail.com>
2020-06-16 16:40:51 -0300

    fix objective function

--- hill-climbing.py
+++ hill-climbing.py
@@ -57,7 +57,7 @@ def check_clone(solution):
 def fitness_function(solution):
 	data = []
 	grau_depen = []
-	for i in range(number_req):
+	for i in range(len(solution)):
 		data.append(bd.iloc[solution[i],0:4])
 		grau_depen.append(data[i].loc['Grau de dependencia'])
 
@@ -70,11 +70,16 @@ def fitness_function(solution):
 	gpd = (gpd/len(solution))
 
 	fit_value, total = 0,0
-	for i in range(number_req):
+	for i in range(len(solution)):
 		fit_value += data[i].loc['Prioridade']
-
-	if sprints_capacity(solution)<capacity_sprints:
-		total = 0.9 * (fit_value - gpd) #with penality
+	
+	#bonus and penality
+	if sprints_capacity(solution) == capacity_sprints:
+		total = (fit_value - gpd) + (0.1 * (fit_value - gpd))
+	elif (0.6*capacity_sprints) >= sprints_capacity(solution) <= (0.9*capacity_sprints):
+		total = 0.9 * (fit_value - gpd)
+	elif sprints_capacity(solution) <= (0.59*capacity_sprints):
+		total = 0.8 * (fit_value - gpd)
 	else:
 		total = fit_value - gpd
 
@@ -107,14 +112,13 @@ def hc_process(iterations):
 			fit_neighbor=fitness_function(sol)
 
 			#print("current solution:", current, "| fit:",str(fit_current), "| estimate/h:", sprints_capacity(current))
-			#print()
 			print("neighbor", sol,"| fit:",str(fit_neighbor),"| estimate/h:", sprints_capacity(current))
 			print()
 			current=sol.copy() if (fit_neighbor<=fit_current) else current	
 		c=c+1
-	print("Final Solution:", current, "| fit:",str(fit_current), "| estimate/h:", sprints_capacity(current))
+		print("New current:", current, "| fit:",str(fit_current), "| estimate/h:", sprints_capacity(current))
 	
 		
 	return current
 
-hc_process(1)
+hc_process(100)

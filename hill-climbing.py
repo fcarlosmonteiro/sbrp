502eb814c738cdab74c8e16ad264b0f9838a3cc7
fcarlosmonteiro <fcarlosmonteiro@gmail.com>
2020-06-16 16:14:12 -0300

    added sprint capacity

--- hill-climbing.py
+++ hill-climbing.py
@@ -3,20 +3,20 @@ from operator import itemgetter
 import pandas as pd
 
 bd = pd.read_csv('base.csv')
-capacity_sprints = 15
+capacity_sprints = 60 #in hours
 
 global number_req
 global iterations
 
 number_req = 4
 
-def create_solution(number_req):
+def create_solution():
 	current = []
-	for i in range(number_req):
+	while sprints_capacity(current)<capacity_sprints:
 		id=1+int(99*random())
 		current.append(id)
-	check_clone(current)
-	#print("current solution: ", current)
+		check_clone(current)
+	print("current solution: ", current)
 	return current
 
 def extends_neighborhood(current):
@@ -55,11 +55,11 @@ def check_clone(solution):
 	return sol
 
 def fitness_function(solution):
-	dados = []
+	data = []
 	grau_depen = []
 	for i in range(number_req):
-		dados.append(bd.iloc[solution[i],0:4])
-		grau_depen.append(dados[i].loc['Grau de dependencia'])
+		data.append(bd.iloc[solution[i],0:4])
+		grau_depen.append(data[i].loc['Grau de dependencia'])
 
 	gpd = 0
 	for g in grau_depen:
@@ -67,34 +67,53 @@ def fitness_function(solution):
 			pass
 		else:
 			gpd += g
-	gpd = (gpd/number_req)
+	gpd = (gpd/len(solution))
 
-	fit_value = 0
+	fit_value, total = 0,0
 	for i in range(number_req):
-		fit_value += dados[i].loc['Prioridade']
-	fit_value -= gpd
+		fit_value += data[i].loc['Prioridade']
 
-	return fit_value
+	if sprints_capacity(solution)<capacity_sprints:
+		total = 0.9 * (fit_value - gpd) #with penality
+	else:
+		total = fit_value - gpd
+
+	return total
 
 def sprints_capacity(solution):
+	data = []
+	data_estimate = []
+	
+	for i in range(len(solution)):
+		
+		data.append(bd.iloc[solution[i],0:5])
+		data_estimate.append(data[i].loc['Estimativa'])
 
-	return True
+	estimate=0
+	for e in data_estimate:
+		if pd.isnull(e):
+			pass
+		else:
+			estimate += e
+	return estimate
 
 def hc_process(iterations):
 	c=0
-	current=create_solution(number_req)
+	current=create_solution()
 	while (c<iterations):
 		neighborhood=extends_neighborhood(current)
 		for sol in neighborhood[0:len(neighborhood)]:
 			fit_current=fitness_function(current)
 			fit_neighbor=fitness_function(sol)
 
-			print("current solution:", current, " fit:",str(fit_current))
-			print("neighbor", sol," fit:",str(fit_neighbor))
-			
+			#print("current solution:", current, "| fit:",str(fit_current), "| estimate/h:", sprints_capacity(current))
+			#print()
+			print("neighbor", sol,"| fit:",str(fit_neighbor),"| estimate/h:", sprints_capacity(current))
+			print()
 			current=sol.copy() if (fit_neighbor<=fit_current) else current	
 		c=c+1
-	print('new current:',current)
+	print("Final Solution:", current, "| fit:",str(fit_current), "| estimate/h:", sprints_capacity(current))
+	
 		
 	return current
 
